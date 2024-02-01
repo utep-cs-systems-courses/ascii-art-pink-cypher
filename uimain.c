fa8b87a48ba519a613d2481e29ade68385044f4d
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-02-01 14:18:08 -0700

    new changes

--- uimain.c
+++ uimain.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "draw.h" // project-related declarations
 
-//testing
+
 int main() 
 {
   puts("Welcome!");
 
   while (1) { // Infinite while loop
 
-    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c) or 'q' to quit\n> ", stdout);
+    fputs("Select which shape you want to print (Triangle = t, Square = s, Chars = c), Arrow = a, or 'q' to quit\n> ", stdout);
     fflush(stdout);		/* stdout only flushes automatically on \n */
     int c;
     while ((c = getchar()) == '\n'); /* ignore newlines */
@@ -29,6 +29,9 @@ int main()
       puts("You selected chars:");
       for (char c = 'A'; c < 'D'; c++)
 	print_char_11x16(c);
+      break;
+    case 'a':
+      puts("You selected arrow:");
       break;
     case 'q':
       puts("Bye!");
