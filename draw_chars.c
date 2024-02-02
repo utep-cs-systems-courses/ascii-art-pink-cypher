3d9ed1bc3d2630caa8a1b875f3be7e37aa95d001
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-02-02 15:34:45 -0700

    plz work pt2

--- draw_chars.c
+++ draw_chars.c
@@ -4,7 +4,7 @@
 void print_char_8x12(char c)
 {
   c -= 0x20;
-  for (char row = 0; row <15; row++) {
+  for (char row = 0; row <12; row++) {
     unsigned short rowBits = font_8x12[c][row];
     for (char col = 0; col < 12; col++) {
       unsigned short colMask = 1 << (11-col); /* mask to select bit associated with bit */
