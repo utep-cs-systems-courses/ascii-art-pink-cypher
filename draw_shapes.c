1a2fe7b7e3b4452cde5ffe6ab6fac76c49246181
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-02-01 17:06:45 -0700

    yuy

--- draw_shapes.c
+++ draw_shapes.c
@@ -28,14 +28,6 @@ void print_triangle(int leftCol, int size)
 
 void print_arrow(int leftCol, int size)
 {
-  int i, j;
-  int endCol = leftCol + size;
-  for (int row = 0; row < size; row++){
-    int col;
-    for (col = 0; col < leftCol; col++) putchar(' ');
-    for (       ; col < endCol;  col++) putchar('*');
-    putchar('\n');
-  }
  for (int row = 0; row <= size; row++) {
     int minCol = leftCol + size - row, maxCol = leftCol + size + row;
     int col;
