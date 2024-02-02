d2d57676cacbf1b5e52a9fa4dabe3c92a7b5e0b7
pink-cypher <143744116+pink-cypher@users.noreply.github.com>
2024-02-02 15:23:15 -0700

    plz work

--- draw.h
+++ draw.h
@@ -8,6 +8,6 @@ void print_char_8x12(char c);
 
 extern const unsigned short font_11x16[95][11];
 extern const unsigned char font_5x7[96][5];
-extern const unsigned char font_8x12[95][12];
+extern const unsigned char font_8x12[95][8];
 
 #endif
--- draw_chars.c
+++ draw_chars.c
@@ -6,7 +6,7 @@ void print_char_8x12(char c)
   c -= 0x20;
   for (char row = 0; row <8; row++) {
     unsigned short rowBits = font_8x12[c][row];
-    for (char col = 0; col < 16; col++) {
+    for (char col = 0; col < 12; col++) {
       unsigned short colMask = 1 << (11-col); /* mask to select bit associated with bit */
       putchar( (rowBits & colMask) ? '*' : ' ');
     }
